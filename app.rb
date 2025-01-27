54f0fce84da7db8ade7b5af71eeb30a2e597d6c6
Aleksei Borovikov <161416353+aborov@users.noreply.github.com>
2025-01-27 21:19:31 +0000

    rework for Winter 2025

--- app.rb
+++ app.rb
@@ -5,6 +5,20 @@ get("/") do
   erb(:homepage)
 end
 
+# get("/dynamic/:number_of_dice/:how_many_sides") do
+#   @num_dice = params.fetch("number_of_dice").to_i
+#   @sides = param.fetch("how_many_sides").to_i
+#   @rolls = []
+
+#   @num_dice.times do
+#     die = rand(1..@sides)
+
+#     @rolls.push(die)
+#   end
+
+#   erb(:flexible)
+# end
+
 get("/dice/:number_of_dice/:how_many_sides") do
   @num_dice = params.fetch("number_of_dice").to_i
 
